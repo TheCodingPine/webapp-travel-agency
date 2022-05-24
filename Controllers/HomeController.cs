dbdbda5056ad04219729a99f28d0cca9f5c30f03
Alberto Rosa <alberto.r.93@hotmail.it>
2022-05-24 16:07:35 +0200

    homepage placeholder

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,84 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using webapp_travel_agency.Data;
 using webapp_travel_agency.Models;
 
 namespace webapp_travel_agency.Controllers
 {
     public class HomeController : Controller
     {
+        
+            [HttpGet]
+            public IActionResult Index()
+            {
+                return View("Index");
+            }
+
+            [HttpGet]
+            public IActionResult Details(int id)
+            {
+                using(webapp_travel_agencyContext db = new webapp_travel_agencyContext())
+                {
+                    try
+                    {
+                        PacchettoViaggio PackFound = db.PacchettoViaggio
+                            .Where(x => x.Id == id)
+                            .Single();  //tanto gli ID non si ripetono!
+                        return View("Details", PackFound);
+                    }
+                    catch (InvalidOperationException ex)
+                    {
+                        return NotFound("Il pacchetto " + id + " non è presente.");
+                    }
+                    catch (Exception ex)
+                    {
+                        return BadRequest(ex.Message);
+                    }
+                }
+            }
+
+
+            [HttpGet]
+            public IActionResult Privacy()
+            {
+                return View();
+            }
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -28,5 +101,10 @@ namespace webapp_travel_agency.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+*/
+
+
     }
 }
